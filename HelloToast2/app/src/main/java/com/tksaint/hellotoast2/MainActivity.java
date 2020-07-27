c99d3c6c3057c5bf8e8c819458cd590a54b48aa5
Saint <60160284@go.buu.ac.th>
2020-07-27 10:41:07 +0700

    Android week02

---
+++ HelloToast2/app/src/main/java/com/tksaint/hellotoast2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tksaint.hellotoast2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private int number = 0;
+private TextView tvCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvCount= findViewById(R.id.txtCountDisplay);
+    }
+    public void showMessage(View v){
+        Toast t = Toast.makeText(this,"Hello Saint",Toast.LENGTH_LONG);
+        t.show();
+    }
+    public void counttUp(View v){
+        String strNumber;
+        number++;
+        strNumber = Integer.toString(number);
+        tvCount.setText(strNumber);
+
+    }
+    public void counttDown(View v){
+        String strNumber;
+        number--;
+        strNumber = Integer.toString(number);
+        tvCount.setText(strNumber);
+
+    }
+}
